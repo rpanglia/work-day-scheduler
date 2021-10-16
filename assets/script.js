f3300a91c5cb41f693e652af753975149cad13c8
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-10-16 01:52:06 -0400

    layout of script and how to retrive user tasks from local storage trial added

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,29 @@
 
 var currentDay = moment().format('dddd, MMMM Do YYYY');
 var currentTime = moment().hour();
+var currentHour = moment().format("HH");
+var currentHourInt = parseInt(currentHour)
 var saveBtn = $(".saveBtn");
 
+// $("#currentDay").html()
 
+
+//Function to start jQuery
+$(document).ready(function () {
+
+    $(".saveBtn").on("click")
+
+
+
+
+
+
+
+
+
+}
+)
+
+
+// saving tasks in localStorage (will need to be added within the function?)
+$("#hour9 .description").val(localStorage.getItem("hour9"));
