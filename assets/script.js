ec7e0aabba89a8996ac3483a0832f44132da3a07
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-10-16 02:17:30 -0400

    added to doc.ready function

--- assets/script.js
+++ assets/script.js
@@ -14,19 +14,25 @@ var saveBtn = $(".saveBtn");
 //Function to start jQuery
 $(document).ready(function () {
 
-    $(".saveBtn").on("click")
+    $(".saveBtn").on("click"), function () {
+        var textInput = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, textInput);
+    }
+
+})
 
 
-
-
-
-
-
-
-
-}
-)
 
 
 // saving tasks in localStorage (will need to be added within the function?)
 $("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
