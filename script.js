9d9494172a10f9360a11d542708999bc852b2d78
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-10-16 11:25:04 -0400

    css not working, moved script and style out of assets--now working

--- script.js
+++ script.js
@@ -4,15 +4,13 @@
 
 var currentDay = moment().format('dddd, MMMM Do YYYY');
 var currentTime = moment().hour();
-var currentHour = moment().format("HH");
-var currentHourInt = parseInt(currentHour)
-var saveBtn = $(".saveBtn");
-
-$("#currentDay").html(currentDay)
-
+// var currentHour = moment().format("HH");
+// var currentHourInt = parseInt(currentHour)
+// var saveBtn = $(".saveBtn");
+$("#currentDay").html(currentDay);
 
 
-// show date AND time in header at top
+//show date AND time in header at top
 $("#currentDay").append();
 function addDate() {
     $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
@@ -24,16 +22,16 @@ setInterval(addDate, 1000);
 //Function to start jQuery
 $(document).ready(function () {
 
-    $(".saveBtn").on("click"), function () {
-        var textInput = $(this).siblings(".description").val();
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        localStorage.setItem(time, textInput);
-    }
+        localStorage.setItem(time, text);
+    })
 
     $(".time-block").each(function() {
         var sectionTime = parseInt($(this).attr("id").split("hour")[1]);
-
+        console.log(sectionTime, currentTime);
         //below code changes the color of the sections as per time class
         if (sectionTime < currentTime) {
             $(this).removeClass("present");
@@ -46,29 +44,25 @@ $(document).ready(function () {
             $(this).addClass("present");
         }
         else {
-            $(this).addClass("future");
             $(this).removeClass("past");
             $(this).removeClass("present"); 
+            $(this).addClass("future");
         }
 
     })
 
 
-
+    // saving tasks in localStorage (will need to be added within the function?)
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
 })
 
-
-
-
-// saving tasks in localStorage (will need to be added within the function?)
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
